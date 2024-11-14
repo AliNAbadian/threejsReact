621cb947bf99964ee6f18c7c99441c6c82854d6a
AliNAbadian <nagshrizali@gmail.com>
2024-11-14 07:48:50 +0330

    Docker Addded

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
-function App() {
-  return <div className="text-3xl">Test</div>;
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+
+function SpinningBox() {
+  const meshRef = useRef();
+
+  useFrame(() => {
+    meshRef.current.rotation.x += 0.01;
+    meshRef.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh ref={meshRef}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="skyblue" />
+    </mesh>
+  );
 }
+
+function App() {
+  return (
+    <Canvas>
+      <ambientLight />
+      <SpinningBox />
+    </Canvas>
+  );
+}
+
 
 export default App;
